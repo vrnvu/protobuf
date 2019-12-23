53e532915397201b81e34071bc486bf70b186325
Arnau Díaz <diazarnau0@gmail.com>
2019-12-23 20:23:33 +0100

    grpc

---
+++ server/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"context"
+	"net"
+
+	"github.com/vrnvu/grpc/proto"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+type Server struct{}
+
+func (s *Server) Add(ctx context.Context, request *proto.Request) (*proto.Response, error) {
+	a, b := request.GetA(), request.GetB()
+
+	result := a + b
+
+	return &proto.Response{Result: result}, nil
+}
+
+func (s *Server) Multiply(ctx context.Context, request *proto.Request) (*proto.Response, error) {
+	a, b := request.GetA(), request.GetB()
+
+	result := a * b
+
+	return &proto.Response{Result: result}, nil
+}
+
+func main() {
+	listener, err := net.Listen("tcp", ":4040")
+	if err != nil {
+		panic(err)
+	}
+
+	srv := grpc.NewServer()
+	proto.RegisterAddServiceServer(srv, &Server{})
+	reflection.Register(srv)
+
+	if e := srv.Serve(listener); e != nil {
+		panic(e)
+	}
+
+}
